1fe201e242664af43e087f419b79cb8e847609ad
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-01 20:49:19 -0300

    Update NukeCommand.js

--- src/commands/moderacion/NukeCommand.js
+++ src/commands/moderacion/NukeCommand.js
@@ -1,3 +1,4 @@
+const { Role } = require('discord.js');
 const BaseCommand = require('../../utils/structures/BaseCommand');
 
 module.exports = class NukeCommand extends BaseCommand {
@@ -9,11 +10,12 @@ module.exports = class NukeCommand extends BaseCommand {
 
     if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("No podes usar este comando");
     if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Ni rol no tiene el permiso de administrar los canales");
-    if (!message.author.hasRole(827322556649439243)) return message.channel.send("No podes usar este comando master");
+    if (!message.author.roles.cache.has(adminRole)) return message.channel.send('No podes usar esto');
+
     let reason = args.join("");
     const nukeChannel = message.channel;
-
-
+    const adminRole = message.guild.roles.cache.get('827322556649439243')
+    
     if (!reason) reason = "No hay razon"
     if (!nukeChannel.deletable) return message.channel.send("Este canal no se puede borrar");
 
