fa47e3f544788fd0959e7e4c86d51b106f2185c1
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-01 10:03:12 -0300

    Update NicknameCommand.js

--- src/commands/moderacion/NicknameCommand.js
+++ src/commands/moderacion/NicknameCommand.js
@@ -9,7 +9,7 @@ module.exports = class NicknameCommand extends BaseCommand {
     if (!message.member.hasPermission("CHANGE_NICKNAME")) return message.channel.send("No podes usar este comando");
     if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("Necesito el permiso de \`MANAGE_NICKNAMES\` para cambiar el nickname");
 
-    const mentionedMember = message.mentions.first() || message.guild.members.cache.get(args[0]);
+    const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     const nickName = args.slice(1).join(" ");
 
     if (!args[0]) return message.channel.send("Tenes que mencionar a un miembro `e!nickname @miembro nickname`");
