189a2f1281b48e9d71d671553d194b63a8bbcf59
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-01 10:00:52 -0300

    Update NicknameCommand.js

--- src/commands/moderacion/NicknameCommand.js
+++ src/commands/moderacion/NicknameCommand.js
@@ -13,5 +13,10 @@ module.exports = class NicknameCommand extends BaseCommand {
     const nickName = args.slice(1).join(" ");
 
     if (!args[0]) return message.channel.send("Tenes que mencionar a un miembro `e!nickname @miembro nickname`");
+    if (!mentionedMember) return message.channel.send("Ese miembro no esta en el server")
+    if (!nickName) return message.channel.send("Tenes que definir un nuevo nick para el miembro. `e!nickname @miembro nickname`");
+    if (!mentionedMember.kickable) return message.channel.send("El rol del miembro es mas altoque el mio");
+
+    await mentionedMember.setNickname(nickName).catch(err => console.log(err) && message.channel.send("No pude cambiar el usuario por un error"));
     }
 }
