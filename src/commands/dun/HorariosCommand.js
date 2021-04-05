c4cce6603a7c2c57ac5b3fb8f79441b3e3e2f38e
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-05 11:00:06 -0300

    r

---
+++ src/commands/dun/HorariosCommand.js
@@ -0,0 +1,23 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+
+module.exports = class HorariosCommand extends BaseCommand {
+  constructor() {
+    super('horarios', 'dun', []);
+  }
+
+  async run(client, message, args) {
+    const horarioEmbed = new Discord.MessageEmbed
+      .setTitle("Horarios")
+      .setDescription("Ed. Judia 8:05 a 9:05, Matematica 9:20 a 10:40, Ingles 10:55 a 12:15")
+      .setFooter(message.author.tag, message.author.displayAvatarURL())
+      .setColor("#0B97B9")
+      .setTimestamp();
+    try {
+        await message.channel.send(horarioEmbed)
+      } catch (err) {
+        console.log(err);
+        message.channel.send('No pude enviar el mensaje');
+    }
+  }
+}
