6c813c74f52c9a1403bb30e1a31a2a57dc21ab56
Defeeeee <fdiaznemeth+2@gmail.com>
2021-04-06 11:48:45 -0300

    Update UnbanCommand.js

--- src/commands/moderacion/UnbanCommand.js
+++ src/commands/moderacion/UnbanCommand.js
@@ -6,13 +6,13 @@ module.exports = class UnbanCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("No tenes permiso master")
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("No tenes permiso master")
     if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("No tengo los permisos necesarios");
     let reason = args.slice(1).join(" ");
     let userID = args[0];
     if (!reason) reason = 'No hay razon'
     if (!args[0]) return message.channel.send('Tenes que especificar a quien queres desbanear `e!unban ID razon`');
-    if (!isNaN(args[0])) return message.channel.send('Ese ID no es un numero. `e!unban ID razon`');
+    if (isNaN(args[0])) return message.channel.send('Ese ID no es un numero. `e!unban ID razon`');
     message.guild.fetchbans().then(async bans => {
       if (bans.size == 0) return message.channel.send('No hay ningun ban en este server');
       let bUser = bans.find(b => b.user.id == userID);
